eb81bb21ed6ed2df95c18865ff384c2b51166fa6
Gregory Lagranha <gregory.lagranha@acad.pucrs.br>
2018-08-31 19:32:57 -0300

    Configuration of sequelize, some models created

--- server.js
+++ server.js
@@ -1,5 +1,14 @@
 var express = require('express');
+var models = require("./app/models");
 var app = express();
+
+//Sync database
+models.sequelize.sync().then(() => {
+    console.log("Simulados database is ready.");
+})
+.catch(function (err) {
+    console.log(err, "Something went wrong while creating simulados_database.")
+});
 
 app.get('/', function (req, res) {
     res.send('Simulados API');

d814d856119dc9510df4e45ea6dd53bfcb5fa532
Ricardo Borges <ricardo93borges@gmail.com>
2018-09-07 21:38:40 -0300

    Created script to populate database with records from a json file

--- app.js
+++ app.js
@@ -20,8 +20,17 @@ var authRoute = require('./app/routes/auth.js')(app);
 var userRoute = require('./app/routes/user.js')(app);
 
 //Sync Database
-// models.sequelize.sync().then(function () {
-//     console.log('You\'re pretty good. Database looks fine')
-// }).catch(function (err) {
-//     console.log(err, "Something wrong is not right with the Database Update!")
-// });
+/*
+models.sequelize.sync().then(function () {
+
+    //Populate database
+    var populate = require('./app/config/populate')
+    populate.run()
+
+    console.log('You\'re pretty good. Database looks fine')
+}).catch(function (err) {
+    console.log(err, "Something wrong is not right with the Database Update!")
+});
+*/
+
+

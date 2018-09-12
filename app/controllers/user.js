88b94badd9c7adb96466e81db761509c78cce9d1
Alan Quadros <alanquadros@hotmail.com>
2018-09-11 22:16:49 -0300

    Refactoring controller user and auth

--- app/controllers/user.js
+++ app/controllers/user.js
@@ -4,7 +4,7 @@ var User = require('../models/user.js')(db.sequelize, db.Sequelize);
 var bCrypt = require('bcrypt-nodejs');
 var validator = require('validator');
 
-exports.create = async function (req, res) {
+exports.create = function (req, res) {
     const body = req.body;
 
     if (!body.email || !validator.isEmail(body.email)) {
@@ -21,7 +21,7 @@ exports.create = async function (req, res) {
                         email: body.email,
                         password: bCrypt.hashSync(body.password, bCrypt.genSaltSync(8), null),
                         name: body.name,
-                        username: body.name
+                        username: body.username
                     };
 
                     User.create(data).then(function (user) {
@@ -34,7 +34,7 @@ exports.create = async function (req, res) {
                 }
             })
         } catch (e) {
-            console.log(e)
+            return res.status(400).json({success: false, error: e.message});
         }
     }
 };
@@ -48,8 +48,8 @@ exports.findAll = function (req, res) {
                 users: users
             })
         })
-    }catch (e) {
-        console.log(e)
+    } catch (e) {
+        return res.status(400).json({success: false, error: e.message});
     }
 };
 
@@ -69,8 +69,8 @@ exports.findOne = function (req, res) {
                 })
             }
         })
-    }catch (e) {
-        console.log(e)
+    } catch (e) {
+        return res.status(400).json({success: false, error: e.message});
     }
 };
 
@@ -112,8 +112,8 @@ exports.update = function (req, res) {
                 })
             })
         }
-    }catch (e) {
-        console.log(e)
+    } catch (e) {
+        return res.status(400).json({success: false, error: e.message});
     }
 };
 
@@ -133,7 +133,7 @@ exports.delete = function (req, res) {
                     })
                 }
             })
-    }catch (e) {
-        console.log(e)
+    } catch (e) {
+        return res.status(400).json({success: false, error: e.message});
     }
 };

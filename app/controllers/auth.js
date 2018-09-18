e8e182d4198cf73fe68c04560cf7ec263441302f
Ricardo Borges <ricardo93borges@gmail.com>
2018-09-17 21:40:58 -0300

    Refactored auth controller

--- app/controllers/auth.js
+++ app/controllers/auth.js
@@ -4,7 +4,12 @@ var User = require('../models/user.js')(db.sequelize, db.Sequelize);
 var bCrypt = require('bcrypt-nodejs');
 var validator = require('validator');
 
-exports.signup = function (req, res) {
+const findUserByEmail = async (email) => {
+    let user = await User.findOne({where: {email: email}})
+    return user
+}
+
+exports.signup = async (req, res) => {
     const body = req.body;
 
     if (!body.email || !validator.isEmail(body.email)) {
@@ -13,34 +18,34 @@ exports.signup = function (req, res) {
         return res.status(400).json({success: false, error: 'Please enter a password to register'});
     } else {
         try {
-            User.findOne({where: {email: body.email}}).then((user) => {
-                if (user) {
-                    return res.status(400).json({success: false, error: 'That email is already taken'});
-                } else {
-                    var data = {
-                        email: body.email,
-                        password: bCrypt.hashSync(body.password, bCrypt.genSaltSync(8), null),
-                        name: body.name,
-                        username: body.username
-                    };
+            let user = await findUserByEmail(body.email)
 
-                    User.create(data).then(function (user) {
-                        res.status(201).json({
-                            success: true,
-                            message: 'Successfully created new user',
-                            user: user.toJSON(),
-                            token: user.getJWT()
-                        })
-                    })
-                }
+            if (user)
+                return res.status(400).json({success: false, error: 'That email is already taken'})
+
+            var data = {
+                email: body.email,
+                password: bCrypt.hashSync(body.password, bCrypt.genSaltSync(8), null),
+                name: body.name,
+                username: body.username
+            };
+
+            User.create(data).then(function (user) {
+                res.status(201).json({
+                    success: true,
+                    message: 'Successfully created new user',
+                    user: user.toJSON(),
+                    token: user.getJWT()
+                })
             })
+
         } catch (e) {
             return res.status(400).json({success: false, error: e.message});
         }
     }
 }
 
-exports.signin = function (req, res) {
+exports.signin = async (req, res) => {
     try {
         const body = req.body
 
@@ -49,38 +54,34 @@ exports.signin = function (req, res) {
         } else if(!body.password) {
             return res.status(400).json({success: false, error: 'Please enter a password to login'});
         } else {
-            User.findOne({where: {email: body.email}}).then(function (user) {
 
-                if (!user)
-                    throw new Error('Email does not exist')
-    
-                if (!bCrypt.compareSync(body.password, user.password))
-                    throw new Error('Incorrect password')
-    
-                let data = {
-                    success: true,
-                    message: 'Successfully login',
-                    user: user.toJSON(),
-                    token: user.getJWT()
-                }
-    
-                res.status(200).json(data)
-    
-            }).catch(function (err) {
-                return {'error':err}
-            })
+            let user = await findUserByEmail(body.email)
+
+            if (!user)
+                throw new Error('Email does not exist')
+
+            if (!bCrypt.compareSync(body.password, user.password))
+                throw new Error('Incorrect password')
+
+            let data = {
+                success: true,
+                message: 'Successfully login',
+                token: user.getJWT()
+            }
+
+            res.status(200).json(data)
         }
     }catch (e) {
         return res.status(400).json({success: false, error: e.message});
     }
 }
 
-exports.dashboard = function (req, res) {
+exports.dashboard = (req, res) => {
     console.log(req)
     res.json(req.user)
 }
 
-exports.logout = function (req, res) {
+exports.logout = (req, res) => {
     req.session.destroy(function (err) {
         res.redirect('/');
     });

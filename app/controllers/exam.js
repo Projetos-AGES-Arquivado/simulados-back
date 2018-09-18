8b7eabdf856a2ac51fa4d8102d5fd44807f6de5e
Ricardo Borges <ricardo93borges@gmail.com>
2018-09-17 21:26:59 -0300

    Refatored exam controller

--- app/controllers/exam.js
+++ app/controllers/exam.js
@@ -6,52 +6,71 @@ var Question = require('../models/question')(db.sequelize, db.Sequelize);
 var ExamQuestion = require('../models/practiseexam_questions')(db.sequelize, db.Sequelize);
 var Student = require('../models/student')(db.sequelize, db.Sequelize);
 
-exports.create = (req, res) => {
-    const body = req.body;
+const findStudentById = async (id) => {
+    let student = await Student.findById(id)
 
-    if (!body.student_id) {
-        return res.status(400).json({success: false, error: 'You must inform student id'});
-    } else {
-        Student.findById(body.student_id).then(async (student) => {
-            if (!student) {
-                return res.status(400).json({success: false, error: 'User not found'});
-            } else {
-                let date = new Date()
+    if (!student)
+        return res.status(400).json({success: false, error: 'User not found'})
 
-                //Fetch questions
-                let questions = await Question.all({where: {approved: 1}})
+    return student
+}
 
-                //Create exam
-                let exam = await Exam.create({aob_exam: true, aob_exam_year: date.getFullYear()})
+const createParticipation = async (student, exam, questions) => {
+    let date = new Date()
 
-                //Add questions
-                await Promise.all(
-                    questions.map((q) => {
-                        return ExamQuestion.create({practise_exam_id: exam.id, question_id: q.id})
-                    })
-                )
+    let participation = await Participation.create(
+        {
+            participation_date: date,
+            time_of_conclusion: null,
+            student_id: student.id,
+            practise_exam_id: exam.id,
+            numberOfQuestions: questions.length,
+            numberOfCorrectAnswers: 0,
+            numberOfWrongAnswers: 0,
+            hitRatio: 0,
+        })
 
-                //Create participation
-                Participation.create(
-                    {
-                        participation_date: date,
-                        time_of_conclusion: null,
-                        student_id: student.id,
-                        practise_exam_id: exam.id,
-                        numberOfQuestions: questions.length,
-                        numberOfCorrectAnswers: 0,
-                        numberOfWrongAnswers: 0,
-                        hitRatio: 0,
-                    })
-                    .then((participation) => {
-                        res.status(201).json({
-                            success: true,
-                            message: 'Successfully created new exam',
-                            participation: participation.toJSON(),
-                            exam: exam.toJSON()
-                        })
-                    })
-            }
+    if (!participation)
+        return res.status(400).json({success: false, error: 'Error on create participation'})
+
+    return participation
+}
+
+
+exports.create = async (req, res) => {
+    try {
+        const body = req.body;
+
+        let date = new Date()
+
+        //get student
+        let student = await findStudentById(body.student_id)
+
+        //Fetch questions
+        let questions = await Question.all({where: {approved: 1}})
+
+        //Create exam
+        let exam = await Exam.create({aob_exam: true, aob_exam_year: date.getFullYear()})
+
+        //Add questions
+        await Promise.all(
+            questions.map((q) => {
+                return ExamQuestion.create({practise_exam_id: exam.id, question_id: q.id})
+            })
+        )
+
+        let participation = await createParticipation(student, exam, questions)
+
+        res.status(201).json({
+            success: true,
+            message: 'Successfully created new exam',
+            participation: participation.toJSON(),
+            exam: exam.toJSON()
+        })
+    }catch (e) {
+        res.status(500).json({
+            success: false,
+            message: e.message
         })
     }
 };

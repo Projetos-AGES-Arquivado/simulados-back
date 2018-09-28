c3a9546983e6cbfcc99e573e7c143970b63fefc3
Edj84 <eduardo.silva.006@acad.pucrs.br>
2018-09-28 17:33:28 -0300

    Fixing async functions. Using await instead of .then

--- app/controllers/participation.js
+++ app/controllers/participation.js
@@ -37,7 +37,8 @@ exports.create = async function (req, res) {
     try {
         // Validating if stundent and practiseExam are on the DB.
         //If they're not, the participation shouldn't exist
-        if (!await Practise_ExamModel.findOne({ where: { id: body.practise_exam_id } })) {
+        let practiseExam = await Practise_ExamModel.findOne({ where: { id: body.practise_exam_id } });
+        if(!practiseExam) {
             return res.status(404).json({ success: false, error: 'Prova não encontrada na base de dados!' });
         }
         let student = await StudentModel.findOne({ where: { id: body.student_id } })
@@ -48,66 +49,58 @@ exports.create = async function (req, res) {
         console.log("Funciona: " + participation_id + "\n" + student_id);
         
         // Check if participation already exists
-        ParticipationModel.findOne({
-            where: { participation_id: body.participation_id }
-        
-        }).then((participation) => {
+        let existingParticipation = await ParticipationModel.findOne({ where: { participation_id: body.participation_id }});
+        if(existingParticipation) {
             // If the participation exists, it's numberOfCorrectAnswers, numberOfWrongAnswers, hitRatio and time_of_conclusion fields should be updated 
-            ParticipationModel.update(
+            let updatedParticipation = await ParticipationModel.update(
                 { numberOfCorrectAnswers: body.numberOfCorrectAnswers, numberOfWrongAnswers: body.numberOfWrongAnswers, hitRatio: body.hitRatio, time_of_conclusion: body.time_of_conclusion },
-                {   where: {
+                { where: {
                         student_id: body.student_id,
                         practise_exam_id: body.practise_exam_id
                         }
-                }
-                    ).then(function (updateStatus) {
-                        // The update function returns 1 if update was successful
-                        if (updateStatus[0] === 1) {
-                            // Since the update function doesn`t return the updated object, we have
-                            // to find it again and return it 
-                            ParticipationModel.findOne({
-                                where: {
-                                    question_id: body.question_id,
-                                    participation_id: body.participation_id,
-                                    alternative_id: body.alternative_id,
-                                    time_to_answer: body.time_to_answer
-                                }
-                            }).then(function (updatedParticipation) {
+                });
+            if(updatedParticipation){
+                // The update function returns 1 if update was successful
+                if (updateStatus[0] === 1) {
+                // Since the update function doesn`t return the updated object, we have to find it again and return it 
+                    let participationModel = await ParticipationModel.findOne({ where: { participation_id: body.participation_id }});
+                    
+                    if(participationModel){
                                 res.status(201).json({
                                     success: true,
                                     message: 'Participação atualizada com sucesso!',
                                     participation: updatedParticipation.toJSON()
                                 });
-                            });
-                        } else {
-                            // Just in case if the updated response returns anything different than 1
-                            return res.status(500).send({
-                                message: "Algo errado com a atualização de uma participação "
-                            });
-                        }
-                    });
+                    } 
+                    
+                    else {
+                        // Just in case if the updated response returns anything different than 1
+                        return res.status(500).send({ message: "Algo errado com a atualização de uma participação " });
+                    }
                 }
+            }            
 
-            */
-
-            } else {
-                // Save new data on database and send it back to the client
-                var data = {
-                    participation_id: body.participation_id,
-                    practise_exam_id: body.practise_exam_id,
-                    student_id: body.student_id,
-
+        } 
+    
+        else {
+            //Save new data on database and send it back to the client
+            var data = {
+                participation_id: body.participation_id,
+                practise_exam_id: body.practise_exam_id,
+                student_id: body.student_id
                 };
-                ParticipationModel.create(data).then(function (participation) {
-                    res.status(201).json({
-                        success: true,
-                        message: 'Participação criada com sucesso!',
-                        participation: participation.toJSON()
-                    });
+
+            ParticipationModel.create(data).then(function (participation) {
+                res.status(201).json({
+                success: true,
+                message: 'Participação criada com sucesso!',
+                participation: participation.toJSON()
                 });
-            }
-        });
-    } catch (err) {
+            });
+        }
+    }
+    
+    catch (err) {
         res.status(500).send({
             'Error': err.message
         });

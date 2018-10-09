182c67f1cda432aca959f2f227dd7a42c50ae8bd
Ricardo Borges <ricardo93borges@gmail.com>
2018-10-08 21:02:00 -0300

    Returning total of questions in getQuestionsWithPagination method

--- app/controllers/question.js
+++ app/controllers/question.js
@@ -7,17 +7,17 @@ var Alternative = require('../models/alternative.js')(db.sequelize, db.Sequelize
 
 exports.getQuestionsWithPagination = async (req, res) => {
 
-    const { examId, lastQuestion = 0, amount = 10 } = req.query
+    const { examId, lastQuestion = 0, amount = 10 } = req.params
 
-    if(!examId) 
-        return res.status(400).json({success: false, error: 'Simulado não informado!'})
+    if (!examId)
+        return res.status(400).json({ success: false, error: 'Simulado não informado!' })
     else {
         //Fetch exam
         Participation.findOne({ where: { practise_exam_id: examId } }).then(async (participation) => {
             if (!participation) {
-                return res.status(401).json({success: false, error: 'Simulado não informado!'})
+                return res.status(401).json({ success: false, error: 'Simulado não informado!' })
             }
-            else if(lastQuestion >= participation.numberOfQuestions) return res.status(400).json({success: false, error: 'No more questions for this exam'})
+            else if (lastQuestion >= participation.numberOfQuestions) return res.status(400).json({ success: false, error: 'No more questions for this exam' })
             else {
 
                 /*===== How to order the questions of exam of OAB? =======*/
@@ -30,24 +30,30 @@ exports.getQuestionsWithPagination = async (req, res) => {
 
                 //Fetch questions
                 let questions = await db.sequelize
-                    .query(query, { type: db.sequelize.QueryTypes.SELECT})
-                    .then(questions => 
-                        db.sequelize.Promise.each(questions, async question => 
+                    .query(query, { type: db.sequelize.QueryTypes.SELECT })
+                    .then(questions =>
+                        db.sequelize.Promise.each(questions, async question =>
                             question.alternatives = await Alternative
-                                .findAll({where: {question_id: question.id}})
+                                .findAll({ where: { question_id: question.id } })
                                 .then(alternatives => alternatives)
                         )
                     )
 
-                if(questions.length)
+                let queryCount = `SELECT q.id FROM questions q INNER JOIN practiseexam_questions pq
+                    ON q.id = pq.question_id WHERE pq.practise_exam_id = ${examId};`
+
+                let questionsTotal = await db.sequelize.query(queryCount, { type: db.sequelize.QueryTypes.SELECT })
+
+                if (questions.length)
                     res.status(200).json({
                         success: true,
                         message: 'Questões resgatadas com sucesso.',
+                        total: questionsTotal.length,
                         questions: questions
                     })
                 else
-                    return res.status(401).json({success: false, error: 'As questões deste simulado não foram encontradas.'})
+                    return res.status(401).json({ success: false, error: 'As questões deste simulado não foram encontradas.' })
             }
-        }).catch(() => res.status(401).json({success: false, error: 'Ocorreu um erro ao buscar as questões.'}))
+        }).catch(() => res.status(401).json({ success: false, error: 'Ocorreu um erro ao buscar as questões.' }))
     }
 }

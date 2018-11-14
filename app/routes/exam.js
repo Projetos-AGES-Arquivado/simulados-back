2f3642a1e24823f9a7b9b740a0bc4bee55541b73
Israel Deorce <israel.deorce@acad.pucrs.br>
2018-11-14 00:01:35 -0200

    get exams working

--- app/routes/exam.js
+++ app/routes/exam.js
@@ -4,9 +4,12 @@ require('../middleware/passport')(passport)
 module.exports = function(app) {
     var examController = require('../controllers/exam.js')
 
-    // Create a new user
-    app.post('/exam', /*passport.authenticate('jwt', {session:false}),*/ examController.create)
+    // Retrieves all exams
+    app.get('/exams', /*passport.authenticate('jwt', {session:false}),*/ examController.findAll)
 
+    // Retrieves all oab exams
     app.get('/exams/oab', /*passport.authenticate('jwt', {session:false}),*/ examController.getOabExams)
 
+    // Create a new exam
+    app.post('/exam', /*passport.authenticate('jwt', {session:false}),*/ examController.create)
 }

e1fb1ff903311acf88119c7e5d97e2e55ce331f8
Edj84 <eduardo.silva.006@acad.pucrs.br>
2018-11-23 21:33:33 -0200

    bug fix

--- app/models/user.js
+++ app/models/user.js
@@ -4,14 +4,41 @@ const jwt = require('jsonwebtoken')
 module.exports = (sequelize, DataTypes) => {
 
     var User = sequelize.define('user', {
-        id: {autoIncrement: true, primaryKey: true, type: DataTypes.INTEGER},
-        name: {type: DataTypes.STRING, notEmpty: true},
-        username: {type: DataTypes.TEXT},
-        about: {type: DataTypes.TEXT},
-        email: {type: DataTypes.STRING, validate: {isEmail: true}},
-        password: {type: DataTypes.STRING, allowNull: false},
-        last_login: {type: DataTypes.DATE},
-        status: {type: DataTypes.ENUM('active', 'inactive'), defaultValue: 'active'}
+        id: {
+            autoIncrement: true, primaryKey: true, type: DataTypes.INTEGER},
+        name: {
+            type: DataTypes.STRING, notEmpty: true
+        },
+        username: {
+            type: DataTypes.TEXT
+        },
+        about: {
+            type: DataTypes.TEXT
+        },
+        email: {
+            type: DataTypes.STRING, validate: {isEmail: true}
+        },
+        password: {
+            type: DataTypes.STRING, allowNull: false
+        },
+        last_login: {
+            type: DataTypes.DATE
+        },
+        status: {
+            type: DataTypes.ENUM('active', 'inactive'), defaultValue: 'active'
+        },
+        created_at: {
+            type: DataTypes.DATE,
+            allowNull: true,
+            defaultValue: DataTypes.NOW
+        },
+        updated_at: {
+            type: DataTypes.DATE,
+            allowNull: true,
+            defaultValue: DataTypes.NOW
+        }
+    }, {
+        underscored: true
     })
 
     User.prototype.getJWT = function () {
